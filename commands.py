35a6bff01a5c745df092a1017e8856f135ba9b64
SobiPacks <40582833+SobiPacks@users.noreply.github.com>
2018-07-11 15:27:00 -0600

    Update commands.py

--- commands.py
+++ commands.py
@@ -1,1 +1,9 @@
 import discord
+from discord.ext import commands
+
+class Commands():
+    def __init__(self, bot):
+        self.bot = bot
+        
+def setup(bot):
+    bot.add_cog(Commands(bot))
